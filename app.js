01db68d5a57bd2824b9f1a2a775f13b163bc8643
mec64 <masonecrane@gmail.com>
2015-05-04 14:12:39 -0600

    created ninjas.html, style.css, and adding some jquery functionality to the ninjas.html page

--- app.js
+++ app.js
@@ -7,8 +7,24 @@ var server = http.createServer(function (request, response){
     // see what URL the clients are requesting:
     console.log('client request URL: ', request.url);
     // this is how we do routing:
+//    console.log('hello');
+//    console.log('REQUEST OBJECT', request);
     if(request.url === '/') {
         fs.readFile('index.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if(request.url === '/style.css') {
+        fs.readFile('style.css', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/css'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if(request.url === '/ninjas') {
+        fs.readFile('ninjas.html', 'utf8', function (errors, contents){
             response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
             response.write(contents);  //  send response body
             response.end(); // finished!
