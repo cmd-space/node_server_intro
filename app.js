7b6b43ec7d94be340656fd78d38a412c82d207b2
mec64 <masonecrane@gmail.com>
2015-05-04 14:27:15 -0600

    gave ninjas.html proper jquery functionality, as well as created /dojos/new route, with a fake form, and customized 404 error text

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var server = http.createServer(function (request, response){
         });
     }
     else if(request.url === '/style.css') {
-        fs.readFile('style.css', 'utf8', function (errors, contents){
+        fs.readFile('./style.css', 'utf8', function (errors, contents){
             response.writeHead(200, {'Content-Type': 'text/css'});  // send data about response
             response.write(contents);  //  send response body
             response.end(); // finished!
@@ -30,10 +30,17 @@ var server = http.createServer(function (request, response){
             response.end(); // finished!
         });
     }
+    else if(request.url === '/dojos/new') {
+        fs.readFile('dojos.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
     // request didn't match anything:
     else {
         response.writeHead(404);
-        response.end('File not found!!!');
+        response.end('URL requested is not available. For recommendations on what should go here please e-mail HR at no@nevergonnahappen.com');
     }
 });
 // tell your server which port to run on
